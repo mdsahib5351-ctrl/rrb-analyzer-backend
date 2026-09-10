f41721b7453b385cf5501318a9aa2c61ea29267e
mdsahib5351-ctrl <mdsahib5351@gmail.com>
2026-09-10 05:40:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -68,3 +68,4 @@ const server=http.createServer(async(req,res)=>{
 server.listen(PORT, '0.0.0.0', () => {
   console.log(`TECH SOURCE RRB Analyzer: http://0.0.0.0:${PORT}`);
 });
+https://tech-source-rrb-backend.onrender.com/api/fetch
